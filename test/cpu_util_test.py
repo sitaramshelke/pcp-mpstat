1ad66ec69fd586abcfc51ff6de46d46f05743d2d
sitaramshelke <sitaramshelke@gmail.com>
2016-07-12 10:55:26 +0530

    Add tests for cpu_util class

---
+++ test/cpu_util_test.py
@@ -0,0 +1,31 @@
+import unittest
+from mock import Mock
+import  mock
+from pcp_mpstat import CpuUtil
+from pcp_mpstat import CoreCpuUtil
+
+class TestCpuUtil(unittest.TestCase):
+
+    def current_values_side_effect(self, metric):
+        if metric == 'hinv.map.cpu_num':
+            return {1: 0, 2: 1}
+
+    def test_get_percpu_util(self):
+        metric_repository = mock.Mock()
+        cpu_util = CpuUtil(1.34, metric_repository)
+        metric_repository.current_values = mock.Mock(side_effect=self.current_values_side_effect)
+
+        cpu_list = cpu_util.get_percpu_util()
+
+        self.assertEquals(len(cpu_list),2)
+    def test_get_totalcpu_util(self):
+        metric_repository = mock.Mock()
+        cpu_util = CpuUtil(1.34, metric_repository)
+        metric_repository.current_values = mock.Mock(side_effect=self.current_values_side_effect)
+
+        cpu_util = cpu_util.get_totalcpu_util()
+
+        self.assertIsNotNone(cpu_util)
+
+if __name__ == '__main__':
+    unittest.main()
