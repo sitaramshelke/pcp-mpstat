f37d19b0926c9f5ab81753ffc5ccf0319970a6ce
sitaramshelke <sitaramshelke@gmail.com>
2016-07-18 11:49:37 +0530

    Add tests for SoftInterruptUsage class

---
+++ test/soft_interrupt_usage_test.py
@@ -0,0 +1,25 @@
+import unittest
+from mock import Mock
+from pcp_mpstat import SoftInterruptUsage
+
+class TestSoftInterruptUsage(unittest.TestCase):
+	def setUp(self):
+		self.metric_repository = Mock()
+		self.metric_repository.current_values = Mock(side_effect = self.current_value_side_effect)
+		self.interrupt_metric = ['kernel.percpu.softirqs.RCU','kernel.percpu.softirqs.HRTIMER','kernel.percpu.softirqs.SCHED']
+	
+	def current_value_side_effect(self, metric):
+		if metric == 'hinv.map.cpu_num':
+			return {'0':0,'1':1,'2':2,'3':3}
+		return None
+	def test_get_percpu_interrupts(self):
+		CpuInterrupts = Mock()
+		Interrupt_usage = Mock()
+		soft_interrupt_usage = SoftInterruptUsage(1.34, self.metric_repository, self.interrupt_metric)
+
+		percpu_interrupts = soft_interrupt_usage.get_percpu_interrupts()
+
+		self.assertEqual(len(percpu_interrupts), 4)
+
+if __name__ == "__main__":
+	unittest.main()
