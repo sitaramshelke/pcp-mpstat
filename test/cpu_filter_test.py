405c8c0b241ad9588abe2427658569b824ec211d
sitaramshelke <sitaramshelke@gmail.com>
2016-07-19 11:08:49 +0530

    Add test for CpuFilter class

---
+++ test/cpu_filter_test.py
@@ -0,0 +1,44 @@
+import unittest
+from mock import Mock
+from pcp_mpstat import CpuFilter
+
+class TestCpuFilter(unittest.TestCase):
+	def setUp(self):
+		self.options = Mock()
+		self.options.cpu_list = None
+		self.cpus = [Mock(
+						cpu_number = Mock(return_value = 0),
+						cpu_online = Mock(return_value = 1)),
+					Mock(
+							cpu_number = Mock(return_value = 1),
+							cpu_online = Mock(return_value = 1)),
+					Mock(
+							cpu_number = Mock(return_value = 2),
+							cpu_online = Mock(return_value = 0)),
+					]
+	def test_filter_cpus_all(self):		
+		self.options.cpu_list = "ALL"
+		cpu_filter = CpuFilter(self.options)
+
+		cpu_list = cpu_filter.filter_cpus(self.cpus)
+
+		self.assertEqual(len(cpu_list), 3)
+
+	def test_filter_cpus_on(self):
+		self.options.cpu_list = "ON"
+		cpu_filter = CpuFilter(self.options)
+
+		cpu_list = cpu_filter.filter_cpus(self.cpus)
+
+		self.assertEqual(len(cpu_list), 2)
+
+	def test_filter_cpus_list(self):
+		self.options.cpu_list = [1]
+		cpu_filter = CpuFilter(self.options)
+
+		cpu_list = cpu_filter.filter_cpus(self.cpus)
+
+		self.assertEqual(len(cpu_list), 1)
+
+if __name__ == '__main__':
+	unittest.main()
